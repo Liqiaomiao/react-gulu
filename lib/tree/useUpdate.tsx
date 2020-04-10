c3ed30074a2a53f79ceb1428a24135dbd37e44c4
Liqiaomiao <1049259456@qq.com>
2020-04-10 09:18:46 +0800

    dev: 封装useUpdate

---
+++ lib/tree/useUpdate.tsx
@@ -0,0 +1,25 @@
+import {useEffect, useState, useRef} from "react";
+
+const useUpdate = (dep:boolean,fn:()=>void)=>{
+    const isFirst = useRef(true)
+    useEffect(()=>{
+        if(isFirst.current){
+            isFirst.current = false
+            return;
+        }
+        fn()
+
+    },[dep]) // 仅在dep更改时更新
+}
+// const useUpdate = (dep:boolean,fn:()=>void)=>{
+//     const [count,setCount] = useState(0)
+//     useEffect(() => {
+//         setCount(x=>x+1)
+//     }, [dep])
+//     useEffect(() => {
+//         if (count >1) {
+//             fn() // created的时候不执行，updated的时候执行
+//         }
+//     }, [count])
+// }
+export default useUpdate
